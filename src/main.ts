e95a21459b5f6614ed8248a4bf4315ddebf5a7fe
Jia Cheng Koh <C200172.e.ntu.edu.sg>
2023-09-17 11:19:48 +0800

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   const port = configService.get('APP_PORT', 3000);
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
